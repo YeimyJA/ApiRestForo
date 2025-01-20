2e5a834eeae0020adaf497db3ecf6027ab5a714f
Wilmer Oidor <yeimya3@gmail.com>
2025-01-19 22:05:27 -0500

    adding topic stuff

--- src/main/java/com/yeimy/forum/controller/TopicController.java
+++ src/main/java/com/yeimy/forum/controller/TopicController.java
@@ -1,7 +1,15 @@
 package com.yeimy.forum.controller;
 
+import java.util.List;
+
+import com.yeimy.forum.model.Topic;
+import com.yeimy.forum.service.TopicService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/topic")
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @GetMapping
-    public String getTopic() {
-        return "Topic!";
+    public List<Topic> getAllTopics() {
+        return topicService.getAllTopics();
+    }
+
+    @GetMapping("/{id}")
+    public Topic getTopicById(@PathVariable Long id) {
+        return topicService.getTopicById(id);
     }
 
     @PostMapping
-    public String createTopic(){
-        return "";
+    public Topic createTopic(@RequestBody Topic topic) {
+        return topicService.createTopic(topic);
     }
 }
---
+++ src/main/java/com/yeimy/forum/service/TopicService.java
@@ -0,0 +1,28 @@
+package com.yeimy.forum.service;
+
+import com.yeimy.forum.model.Topic;
+import com.yeimy.forum.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    public List<Topic> getAllTopics() {
+        return topicRepository.findAll();
+    }
+
+    public Topic getTopicById(Long id) {
+        return topicRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Topic not found with id: " + id));
+    }
+
+    public Topic createTopic(Topic topic) {
+        return topicRepository.save(topic);
+    }
+}
